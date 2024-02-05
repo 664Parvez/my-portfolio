edff30d5a547c3d2d2e5ce0982dc52c6ef28e3b7
unknown <parvezrabbi664@gmail.com>
2024-02-06 01:35:22 +0600

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link"
+import {motion} from "framer-motion"
 
 import homeStyle from "./page.module.css";
 
@@ -23,36 +26,78 @@ import project2 from "../../public/image/home/project2.png"
 import project4 from "../../public/image/home/project4.png"
 
 export default function Home() {
+
+  const fontUp = {
+    start: {
+        y: 100,
+        opacity: 0
+    },
+    end: {
+        y: 0,
+        opacity: 1
+    }
+}
+
   return (
     <>
       <section id={homeStyle.hero_section}>
         <div className="row">
           <div className="col-lg-6">
 
-            <h1 id={homeStyle.hero_first_title}>
+            <motion.h1
+              initial="start"
+              animate="end"
+              variants={fontUp}
+              transition={{duration: .5}}
+            id={homeStyle.hero_first_title}>
               Say Hi from <mark>Parvez,</mark>
-            </h1>
+            </motion.h1>
 
-            <h1 id={homeStyle.hero_second_title}>
+            <motion.h1
+              initial="start"
+              animate="end"
+              variants={fontUp}
+              transition={{duration: .5, delay: .4}}
+            id={homeStyle.hero_second_title}>
               Webflow Designer
-            </h1>
+            </motion.h1>
 
             <div className="row align-items-center">
               <div className="col-lg-6">
-                <p id={homeStyle.strategyText}>Just a Design and Strategy Agency with an aduptive approach to problem solving</p>
+                <motion.p
+                  initial={{color: "#555555"}}
+                  animate={{color: "#ffffff"}}
+                  transition={{duration: .5, delay: .8}}
+                id={homeStyle.strategyText}>Just a Design and Strategy Agency with an aduptive approach to problem solving</motion.p>
               </div>
               <div className="col-lg-6">
-                <h1 id={homeStyle.hero_third_title}>
+                <motion.h1
+                  initial="start"
+                  animate="end"
+                  variants={fontUp}
+                  transition={{duration: .5, delay: 1}}
+                id={homeStyle.hero_third_title}>
                   & Developer
-                </h1>
+                </motion.h1>
               </div>
             </div>
 
-            <p className="mt-4">I design and code beautifully simple things and i love what i do. Just simple like that!</p>
+            <motion.p
+              initial="start"
+              animate="end"
+              variants={fontUp}
+              transition={{duration: .5, delay: 1.2}}
+            className="mt-4">I design and code beautifully simple things and i love what i do. Just simple like that!</motion.p>
 
           </div>
           <div className="col-lg-6">
-            <Image src={myself} width="0" height="0" layout="responsive" alt="" />
+            <motion.div
+              initial={{scale: 0, opacity: 0}}
+              animate={{scale: 1, opacity: 1}}
+              transition={{duration: 1}}
+            >
+              <Image src={myself} loading="lazy" width="0" height="0" layout="responsive" alt="" />
+            </motion.div>
           </div>
         </div>
       </section>
@@ -62,23 +107,45 @@ export default function Home() {
           <div className="col-lg-6">
             <div className="row">
               <div className="col-lg-6 col-md-6">
-                <h2>4+</h2>
-                <p>YEARS OF EXPERIENCE</p>
+                <motion.h2
+                  initial="start"
+                  whileInView="end"
+                  variants={fontUp}
+                  transition={{duration: .5}}
+                >4+</motion.h2>
+                <motion.p
+                  initial={{x: 100, opacity: 0}}
+                  whileInView={{x: 0, opacity: 1}}
+                  transition={{duration: .5, delay: .4}}
+                >YEARS OF EXPERIENCE</motion.p>
               </div>
               <div className="col-lg-6 col-md-6">
-                <h2>182+</h2>
-                <p>PROJECTS COMPLETED ON 15 COUNTRIES</p>
+                <motion.h2
+                  initial="start"
+                  whileInView="end"
+                  variants={fontUp}
+                  transition={{duration: .5}}
+                >182+</motion.h2>
+                <motion.p
+                  initial={{x: 100, opacity: 0}}
+                  whileInView={{x: 0, opacity: 1}}
+                  transition={{duration: .5, delay: .8}}
+                >PROJECTS COMPLETED ON 15 COUNTRIES</motion.p>
               </div>
             </div>
           </div>
           <div className="col-lg-6 d-flex justify-content-center">
               <Link href="#page_portfolio_section__uNVS7">
-                <div className={homeStyle.roundImg}>
+                <motion.div
+                  initial={{scale: 0, opacity: 0}}
+                  whileInView={{scale: 1, opacity: 1}}
+                  transition={{duration: .5}}
+                className={homeStyle.roundImg}>
                     <div className="position-absolute">
                       <FaLongArrowAltDown id={homeStyle.arrowIcon} />
                     </div>
                     <Image height="0" width="0" layout="responsive" src={roundImg} alt="" />
-                </div>
+                </motion.div>
               </Link>
           </div>
         </div>
@@ -87,58 +154,122 @@ export default function Home() {
       <section id={homeStyle.third_section}>
         <div className="row">
           <div className="col-lg-6">
-            <p><FaRegUser /> ABOUT ------------------- </p>
-            <h2>Every great design begin with an even better story</h2>
-            <p className="mb-5">Since beginning my journey as a freelance designer nearly 8 years ago, I have done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I am quietly confident, naturally curious, and perpetually working on improving my chopsone design problem at a time.</p>
+            <motion.p
+              initial={{x: -50, opacity: 0}}
+              whileInView={{x: 0, opacity: 1}}
+              transition={{duration: .5}}
+            ><FaRegUser /> ABOUT ------------------- </motion.p>
+            <motion.h2
+              initial="start"
+              whileInView="end"
+              variants={fontUp}
+              transition={{duration: .5}}
+            >Every great design begin with an even better story</motion.h2>
+            <motion.p 
+              initial="start"
+              whileInView="end"
+              variants={fontUp}
+              transition={{duration: .5, delay: .4}}
+            className="mb-5">Since beginning my journey as a freelance designer nearly 8 years ago, I have done remote work for agencies, consulted for startups, and collaborated with talented people to create digital products for both business and consumer use. I am quietly confident, naturally curious, and perpetually working on improving my chopsone design problem at a time.</motion.p>
           </div>
           <div className="col-lg-6">
             <div className="text-end">
-              <p className={homeStyle.thirdLink}><Link href="">Read More About Me <FiArrowDownRight /></Link></p>
+              <motion.p
+                initial={{y: 50, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: .6}}
+              className={homeStyle.thirdLink}><Link href="">Read More About Me <FiArrowDownRight /></Link></motion.p>
             </div>
           </div>
         </div>
       </section>
 
       <section id={homeStyle.portfolio_section}>
-            <p><RxDragHandleDots2 /> PORTFOLIO ------------------- </p>
-            <h2>Featured Projects</h2>
+            <motion.p
+              initial={{x: -50, opacity: 0}}
+              whileInView={{x: 0, opacity: 1}}
+              transition={{duration: .5}}
+            ><RxDragHandleDots2 /> PORTFOLIO ------------------- </motion.p>
+            <motion.h2
+              initial="start"
+              whileInView="end"
+              variants={fontUp}
+              transition={{duration: .5, delay: .4}}
+            >Featured Projects</motion.h2>
 
             <div className="row">
               <div className="col-lg-2"></div>
               <div className="col-lg-8 my-4">
                 <Link href="https://664parvez.github.io/GSAP-website-one/">
-                  <Image src={project1} width="0" height="0" layout="responsive" alt="" />
+                  <motion.div 
+                    initial={{scale: 0, opacity: 0}}
+                    whileInView={{scale: 1, opacity: 1}}
+                    transition={{duration: .3, delay: .2}}
+                  >
+                    <Image src={project1} loading="lazy" width="0" height="0" layout="responsive" alt="" />
+                  </motion.div>
                 </Link>
                 <h3 className="mt-4">Expanet - Service Provider</h3>
               </div>
               <div className="col-lg-2"></div>
               <div className="col-lg-6 my-4">
                 <Link href="https://addison-interior.netlify.app/">
-                  <Image src={project2} width="0" height="0" layout="responsive" alt="" />
+                  <motion.div 
+                    initial={{scale: 0, opacity: 0}}
+                    whileInView={{scale: 1, opacity: 1}}
+                    transition={{duration: .3, delay: .4}}
+                  >
+                    <Image src={project2} loading="lazy" width="0" height="0" layout="responsive" alt="" />
+                  </motion.div>
                 </Link>
                 <h3 className="mt-4">Addison - Interior Service</h3>
               </div>
               <div className="col-lg-6 my-4">
                 <Link href="https://get-course.vercel.app/">
-                  <Image src={project4} width="0" height="0" layout="responsive" alt="" />
+                  <motion.div 
+                    initial={{scale: 0, opacity: 0}}
+                    whileInView={{scale: 1, opacity: 1}}
+                    transition={{duration: .3, delay: .4}}
+                  >
+                    <Image src={project4} loading="lazy" width="0" height="0" layout="responsive" alt="" />
+                  </motion.div>
                 </Link>
                 <h3 className="mt-4">hi Study - Education/Courses Portal</h3>
               </div>
             </div>
 
             <div className="text-center">
-              <Link href="portfolio" id={homeStyle.portfoli_btn} className="btn btn-lg">See More <FiArrowUpRight /></Link>
+              <motion.div
+                initial={{scale: 0, opacity: 0}}
+                whileInView={{scale: 1, opacity: 1}}
+                transition={{duration: .3}}
+              >
+                <Link href="portfolio" id={homeStyle.portfoli_btn} className="btn btn-lg">See More <FiArrowUpRight /></Link>
+              </motion.div>
             </div>
       </section>
 
       <section id={homeStyle.price_section}>
-        <p><FaDollarSign/> PRICING ------------------- </p>
-        <h2 className="mb-5" id={homeStyle.priceTitle}>My Pricing</h2>
+        <motion.p
+          initial={{x: -50, opacity: 0}}
+          whileInView={{x: 0, opacity: 1}}
+          transition={{duration: .5}}
+        ><FaDollarSign/> PRICING ------------------- </motion.p>
+        <motion.h2
+          initial="start"
+          whileInView="end"
+          variants={fontUp}
+          transition={{duration: .5, delay: .4}}
+        className="mb-5" id={homeStyle.priceTitle}>My Pricing</motion.h2>
 
         <div className="row">
 
           <div className="col-lg-4 mt-3">
-            <div id={homeStyle.priceDiv}>
+            <motion.div
+              initial={{y: 100, opacity: 0}}
+              whileInView={{y: 0, opacity: 1}}
+              transition={{duration: .3}}
+            id={homeStyle.priceDiv}>
               <div className="row">
                 <div className="col-6">
                   <h4><b>Basic</b></h4>
@@ -161,11 +292,15 @@ export default function Home() {
                 </ul>
 
                 <Link href="/contact" className="btn btn-lg">Get This Package</Link>
-            </div>
+            </motion.div>
           </div>
 
           <div className="col-lg-4 mt-3">
-            <div id={homeStyle.priceDiv}>
+            <motion.div
+              initial={{y: 100, opacity: 0}}
+              whileInView={{y: 0, opacity: 1}}
+              transition={{duration: .3, delay: .2}}
+            id={homeStyle.priceDiv}>
               <div className="row">
                 <div className="col-6">
                   <h4><b>Standard</b></h4>
@@ -188,11 +323,15 @@ export default function Home() {
                 </ul>
 
                 <Link href="/contact" className="btn btn-lg">Get This Package</Link>
-            </div>
+            </motion.div>
           </div>
 
           <div className="col-lg-4 mt-3">
-            <div id={homeStyle.priceDiv}>
+            <motion.div
+              initial={{y: 100, opacity: 0}}
+              whileInView={{y: 0, opacity: 1}}
+              transition={{duration: .3, delay: .4}}
+            id={homeStyle.priceDiv}>
               <div className="row">
                 <div className="col-6">
                   <h4><b>Primeum</b></h4>
@@ -215,53 +354,105 @@ export default function Home() {
                 </ul>
 
                 <Link href="/contact" className="btn btn-lg">Get This Package</Link>
-            </div>
+            </motion.div>
           </div>
         </div>
         <p className="mt-5">Don't find any package match with your plan! Want to setup a new tailor-made package for only you? <Link href="/contact" id={homeStyle.price_section_contact}>Contact Me</Link></p>
       </section>
 
       <section id={homeStyle.brand_section}>
-        <h2>Work With <span>60+</span> Brands Worldwide</h2>
+        <h2>Work With <motion.span
+          initial={{scale: 0, opacity: 0}}
+          whileInView={{scale: 1, opacity: 1}}
+          transition={{duration: .3, delay: .4}}
+        >60+</motion.span> Brands Worldwide</h2>
         <div className="row text-center d-flex justify-content-center">
           <div className="col-lg-3 col-md-4 mt-4">
             <Link href="https://artevointerior.com/">
-              <Image src={clogo1} width="0" height="0" layout="responsive" alt=""/>
+              <motion.div
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: .2}}
+              >
+                <Image src={clogo1} loading="lazy" width="0" height="0" layout="responsive" alt=""/>
+              </motion.div>
             </Link>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <Link href="http://www.beonscale.com/">
-              <Image src={clogo2} width="0" height="0" layout="responsive" alt=""/>
+              <motion.div
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: .4}}
+              >
+                <Image src={clogo2} loading="lazy" width="0" height="0" layout="responsive" alt=""/>
+              </motion.div>
             </Link> 
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <Link href="https://buynsell.jamidaribd.com/">
-              <Image src={clogo3} width="0" height="0" layout="responsive" alt=""/>
+              <motion.div
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: .6}}
+              >
+                <Image src={clogo3} loading="lazy" width="0" height="0" layout="responsive" alt=""/>
+              </motion.div>
             </Link>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <Link href="https://outsourcingtrainingbd.com/">
-              <Image src={clogo4} width="0" height="0" layout="responsive" alt=""/>
+              <motion.div
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: .8}}
+              >
+                <Image src={clogo4} loading="lazy" width="0" height="0" layout="responsive" alt=""/>
+              </motion.div>
             </Link>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <Link href="https://www.ukdreamseducation.com/">
-              <Image src={clogo5} width="0" height="0" layout="responsive" alt=""/>
+              <motion.div
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: 1}}
+              >
+                <Image src={clogo5} loading="lazy" width="0" height="0" layout="responsive" alt=""/>
+              </motion.div>
             </Link>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <Link href="https://sr.enterprise.ajaagro.com/">
-              <Image src={clogo6} width="0" height="0" layout="responsive" alt=""/>
+              <motion.div
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: 1.2}}
+              >
+                <Image src={clogo6} loading="lazy" width="0" height="0" layout="responsive" alt=""/>
+              </motion.div>
             </Link>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <Link href="https://clipping-path-website-final.vercel.app/">
-              <Image src={clogo7} width="0" height="0" layout="responsive" alt=""/>  
+              <motion.div
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: 1.4}}
+              >
+                <Image src={clogo7} loading="lazy" width="0" height="0" layout="responsive" alt=""/>  
+              </motion.div>
             </Link>
           </div>
           <div className="col-lg-3 col-md-4 mt-4">
             <Link href="https://dms-live-tv.vercel.app/">
-              <Image src={clogo8} width="0" height="0" layout="responsive" alt=""/>
+              <motion.div
+                initial={{y: 100, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{duration: .3, delay: 1.6}}
+              >
+                <Image src={clogo8} loading="lazy" width="0" height="0" layout="responsive" alt=""/>
+              </motion.div>
             </Link>
           </div>
         </div>
